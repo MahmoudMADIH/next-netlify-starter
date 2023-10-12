8d4eef9449d783bd8921f0cebe1becadbfa6809b
MahmoudMADIH <101363586+MahmoudMADIH@users.noreply.github.com>
2023-10-13 02:39:38 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,27 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+   <!DOCTYPE html>
+<html>
+<head>
+  <title>Download Our App</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+  <div class="container">
+    <h1>Welcome to install our app!</h1>
+    <h2>  
+    </h2>
+    <p>Please press the button below to download the app.</p>
+    <h3>   
+    </h3>
+    <a href="..\dawnload_app\J-NovelClub.apk" download="app.apk">Download</a>
+  </div>
+</body>
+</html>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
   )
 }
